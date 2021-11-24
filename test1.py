c31053eb7e4ddcc838f57eb9a6184daf0af61a95
lingtao <lingtao.lt@antgroup.com>
2021-11-24 14:58:50 +0800

    update branch2 test

--- test1.py
+++ test1.py
@@ -1,4 +1,6 @@
-print('fancy')
-# asdjlkalakd
-# jklsjfds
-# kkalk
+# __name__ == '__main__':
+# xy_center_list, center_x, img = ss_nomix(img_path="./images/rewards.png")
+# for xy_center in xy_center_list:
+#     if 10 < abs(xy_center[0] - center_x) < 15:
+#         # xy_center(x,y,w,h)
+#         print(xy_center)
